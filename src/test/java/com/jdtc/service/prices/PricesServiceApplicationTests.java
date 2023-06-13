96261a722dfb2df9805751fc151af9cf77070a0c
Johan Torres <ingeniero204@gmail.com>
2023-06-13 05:48:03 -0500

    Initial commit

---
+++ src/test/java/com/jdtc/service/prices/PricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdtc.service.prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
